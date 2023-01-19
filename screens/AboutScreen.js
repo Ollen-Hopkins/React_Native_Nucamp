9b3fcaac5b8f983426a5f121c793108559250185
Ollen Hopkins <hopkins_ollen@yahoo.com>
2023-01-18 22:06:00 -0500

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,10 +2,36 @@ import { ScrollView, Text } from "react-native";
 import { Avatar, Card, ListItem } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if(partners.isLoading){
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        )
+    }
+
+    if(partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        )
+    }
 
     return (
         <ScrollView>
@@ -13,17 +39,17 @@ const AboutScreen = () => {
             <Card>
                 <Card.Title>Community Partners</Card.Title>
                 <Card.Divider />
-                {partners.partnersArray.map((partner) => {
-                    return(
-                        <ListItem key={partner.id}>
-                            <Avatar rounded source={{ uri: baseUrl + partner.image}} />
-                            <ListItem.Content>
-                                <ListItem.Title>{partner.name}</ListItem.Title>
-                                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
-                    )
-                })}
+                {partners.partnersArray.map((partner) => (
+                    <ListItem key={partner.id}>
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + partner.image }} />
+                        <ListItem.Content>
+                            <ListItem.Title>{partner.name}</ListItem.Title>
+                            <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                ))}
             </Card>
         </ScrollView>
     )
@@ -36,11 +62,11 @@ const Mission = () => {
             <Card.Title>Our Mission</Card.Title>
             <Card.Divider />
             <Text style={{ margin: 10 }}>
-                We present a curated database of the best campsites in the vast woods 
-                and backcountry of the World Wide Web Wilderness. We increase access to 
-                adventure for the public while promoting safe and respectful use of resources. 
-                The expert wilderness trekkers on our staff personally verify each campsite to make 
-                sure that they are up to our standards. We also present a platform for 
+                We present a curated database of the best campsites in the vast woods
+                and backcountry of the World Wide Web Wilderness. We increase access to
+                adventure for the public while promoting safe and respectful use of resources.
+                The expert wilderness trekkers on our staff personally verify each campsite to make
+                sure that they are up to our standards. We also present a platform for
                 campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
