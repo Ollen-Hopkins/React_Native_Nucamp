20f1ca0c9b4ac22dd2a4b497d7183752962fbfd3
Ollen Hopkins <hopkins_ollen@yahoo.com>
2023-01-26 22:23:17 -0500

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
+import Main from './screens/MainComponent';
 
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Main />
-      </NavigationContainer>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <NavigationContainer>
+          <Main />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
